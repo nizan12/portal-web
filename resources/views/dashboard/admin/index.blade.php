4715a5f8d2fc879001737ed389a98e53227f7f72
nizan12 <nizan1258@gmail.com>
2026-06-02 00:16:16 +0700

    update

--- resources/views/dashboard/admin/index.blade.php
+++ resources/views/dashboard/admin/index.blade.php
@@ -246,7 +246,7 @@
                     <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"></polygon>
                 </svg>
                 <span style="font-size: 12px; font-weight: 600; color: #b45309; line-height: 1.4;">
-                    Kategori Teraktif Utama (Hasil Stored Procedure): <strong>{{ $statsDetail['mostActiveCategoryFromProc'] }}</strong>
+                    Kategori Teraktif Utama : <strong>{{ $statsDetail['mostActiveCategoryFromProc'] }}</strong>
                 </span>
             </div>
         </div>
